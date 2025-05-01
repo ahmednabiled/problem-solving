2753dc084e55adf1026e4b8ee65ef37a74733ebb
Ahmed Nabil <hhtg250@gmail.com>
2025-05-02 01:41:29 +0300

    solved q9 sh6 lvl0

---
+++ icpc-level-0/sheet-6-STLs2/i-unique-elements.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void IO() {
+    #ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+    #endif
+}
+
+void fastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+
+void solve() {
+    int n; cin >> n;
+    set<int> save;
+    set<int> reject;
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x; cin >> x;
+        if(!save.count(x) && !reject.count(x)){
+            save.insert(x);
+        }
+        else if(save.count(x)){
+            save.erase(x);
+            reject.insert(x);
+        }
+    }
+    cout << save.size() << "\n";
+}
+
+signed main() {
+    fastIO();
+    IO();
+    // int t; cin >> t; while(t--)
+    solve();
+    return 0;
+}
